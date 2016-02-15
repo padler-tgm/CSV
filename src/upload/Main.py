25a8b29f09e9d139ca91bbab24bcdde99da854b2
Philipp Adler <padler@student.tgm.ac.at>
2016-02-15 11:49:11 +0100

    fixed imports

--- src/upload/Main.py
+++ src/upload/Main.py
@@ -1,21 +1,14 @@
 import csv
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+#from PyQt5.QtCore import *
+#from PyQt5.QtWidgets import *
+#from PyQt5.QtGui import *
 from CSV.src.upload import View, Model
 import sys
 
+"""
 class Controller(QWidget):
-    """
-    Controller verknuepft das Model mit der View, stellt ein CSV-File grafisch dar
-    @author Philipp Adler
-    @version 2016-01-16
-    """
+
     def __init__(self, parent=None):
-        """
-        Konstruktor der die Gui mit dem Model verbindet und die Applikation startet
-        :param parent optionaler Parameter
-        """
         super().__init__(parent)
         self.myForm = View.Ui_Form()#holt sich die View
         self.myForm.setupUi(self)
@@ -23,9 +16,6 @@ class Controller(QWidget):
         self.myForm.pushButton.clicked.connect(self.filedialog)#Listener auf den Button
 
     def filedialog(self):
-        """
-        Oeffnet einen FileDialog, wo der Benutzer ein CSV-File auswaehlt und inizialsiert die Tabelle mit dem Inhalt des Files
-        """
         message = QFileDialog()
         csv_path = message.getOpenFileName(self, "Open file", "", "CSV (*.csv);;")[0]
         if csv_path != '':
@@ -33,16 +23,11 @@ class Controller(QWidget):
                 self.setTableView(csv_reader(file))
 
     def setTableView(self, list):
-        """
-        Hier wird die TableView inizialisiert
-        :param die Liste welche in der Tabelle dargestellt wird
-        """
         self.dm = Model.ModelTableview(list)
         self.myForm.tableView.setModel(self.dm)
         self.myForm.tableView.verticalHeader().setDefaultSectionSize(15);
         self.myForm.tableView.horizontalHeader().setDefaultSectionSize(100);
-
-
+"""
 #----------------------------------------------------------------------
 def csv_reader(file_obj, test = False):
     """
@@ -64,7 +49,7 @@ if __name__ == "__main__":
     """
     with open('Datei1.csv', newline='') as csvfile:
         csv_reader(csvfile, True)
-    app = QApplication(sys.argv)
-    c = Controller()
-    c.show()
-    sys.exit(app.exec_())
+    #app = QApplication(sys.argv)
+    #c = Controller()
+    #c.show()
+    #sys.exit(app.exec_())
