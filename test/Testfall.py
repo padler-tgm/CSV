83d837d16d9507fe0f563c76692853bb8911631d
Philipp Adler <padler@student.tgm.ac.at>
2016-02-16 08:54:11 +0100

    blabl

--- test/Testfall.py
+++ test/Testfall.py
@@ -17,22 +17,22 @@ def csv_reader(file_obj, test = False):
 
 class TestAllgemein(unittest.TestCase):
     def test_empytCSV(self):
-        with open('empty.csv', newline='') as csvfile:
+        with open('./empty.csv', newline='') as csvfile:
             self.assertEqual(csv_reader(csvfile), [])
 
     def test_noCSV(self):
             self.assertRaises(FileNotFoundError, open, '', newline='')
 
     def test_normalCSV_1row(self):
-        with open('Datei1.csv', newline='') as csvfile:
+        with open('./Datei1.csv', newline='') as csvfile:
             self.assertEqual(csv_reader(csvfile), [['1', '1']])
 
     def test_normalCSV_2row(self):
-        with open("Datei.csv", newline='') as csvfile:
+        with open("./Datei.csv", newline='') as csvfile:
             self.assertEqual(csv_reader(csvfile), [['1', '1'], ['2', '2']])
 
     def test_delimiter(self):
-        with open('Datei3.csv', newline='') as csvfile:
+        with open('./Datei3.csv', newline='') as csvfile:
             self.assertIsNot(csv_reader(csvfile), [['1', '1']])
 
 if __name__ == '__main__':
